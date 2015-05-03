0220a706e5523a7b70befa00f101dbbd7f6d4595
Leviy <ya.tema-000@yandex.ru>
2015-05-03 14:29:10 +0300

    qq

--- main.py
+++ main.py
@@ -44,6 +44,38 @@ if param < 18.0:
 	finish.append(r2)
 length = len(finish)
 print(length)
+p = 0
+while p < 100:
+	u = 0
+	r3 =[]
+	pi = 0
+	normal = []
+	while u < len(finish):
+		q = []
+		random.shuffle(s)
+		y = 0
+		while y < 300:
+			q.append(s[y])
+			y = y + 1
+		r3 = genmat.genmat(q)
+		r2 = finish[u]
+		param = khi.khi2(r2,r3)
+		file1.write("Khi = "+str(param)+"\n")
+		if param < 19.4:
+			pi = pi + 1
+		else:
+			break
+		u = u + 1
+	if pi >= len(finish):
+		finish.append(r3)
+		m3 = norm.norm(r3, 300.0)
+		file2.write(str(m3)+"\n")
+		file1.write("\n"+"++")
+	else:
+		file1.write("\n"+"--")
+	if p%10==0:
+		print(p)
+	p = p + 1
 fin = time.time()
 print(fin-start)
 print(len(finish))
